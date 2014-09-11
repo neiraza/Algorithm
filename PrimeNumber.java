2f63ca79287289957c16c765e2f26fee8e34c92d
togu <tooru.oguri@gmail.com>
2014-09-11 22:56:46 +0900

    素数はやっぱり素数でしょ的な

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,12 +1,10 @@
 public class PrimeNumber {
     public static void main(String args[]) {
-
-        // nまでの素数を出力する
         int maxNum = 100;
         for (int i = 2; i <= maxNum; i++) {
             boolean isPrime = true;
-            for (int j = 2; j <= i; j++) {
-                if (i != j && i % j == 0) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
                     isPrime = false;
                     break;
                 }
